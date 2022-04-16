ea39cea010a99711b170391c8c2436b4a755f825
Wout van Helvoirt <woutvh@duckmade.io>
2022-04-16 21:35:08 +0200

    Updated all compose files and deleted unused ones

--- portainer.sh
+++
@@ -1,9 +0,0 @@
-docker run -d \
---name "portainer_app_1" \
---network "bridge" \
---restart "unless-stopped" \
--p "8000:8000" \
--p "9000:9000" \
--v "/var/run/docker.sock:/var/run/docker.sock" \
--v "/mnt/user/appdata/portainer:/data" \
-portainer/portainer-ce:latest
